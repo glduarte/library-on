00481642bbb71fbd5c1d85ba871c62fd329fd329
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-05 01:57:36 -0300

    Add update route

--- src/modules/books/book.routes.ts
+++ src/modules/books/book.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import CreateBookController from "./controllers/createBook.controller";
 import ShowBooksController from "./controllers/showBooks.controller";
+import UpdateBookController from "./controllers/updateBook.controller";
 
 const createBookController = new CreateBookController();
 const showBooksController = new ShowBooksController();
+const updateBookController = new UpdateBookController();
 
 const bookRouter = Router();
 
 bookRouter.get("/", showBooksController.handle);
 
 bookRouter.post("/", createBookController.handle);
+
+bookRouter.patch("/:id", updateBookController.handle);
 
 export default bookRouter;
