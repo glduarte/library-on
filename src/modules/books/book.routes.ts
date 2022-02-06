41cd809e70b249dbbfb0674e0a7cc3ba0cb42a71
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-06 02:25:18 -0300

    Add delete book route

--- src/modules/books/book.routes.ts
+++ src/modules/books/book.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import CreateBookController from "./controllers/createBook.controller";
+import DeleteBookController from "./controllers/deleteBook.controller";
 import ShowBooksController from "./controllers/showBooks.controller";
 import UpdateBookController from "./controllers/updateBook.controller";
 
 const createBookController = new CreateBookController();
 const showBooksController = new ShowBooksController();
 const updateBookController = new UpdateBookController();
+const deleteBookController = new DeleteBookController();
 
 const bookRouter = Router();
 
@@ -14,5 +16,7 @@ bookRouter.get("/", showBooksController.handle);
 bookRouter.post("/", createBookController.handle);
 
 bookRouter.patch("/:id", updateBookController.handle);
+
+bookRouter.delete("/:id", deleteBookController.handle);
 
 export default bookRouter;
