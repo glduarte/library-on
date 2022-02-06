bbaccb01909263bc0fe67e52cee88544d5fefe03
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-06 02:25:00 -0300

    Add delete book service

---
+++ src/modules/books/services/deleteBook.service.ts
@@ -0,0 +1,18 @@
+import Service from "../../../shared/abstract_classes/service";
+import CustomError from "../../../shared/customError";
+import prisma from "../../../shared/prisma";
+
+export default class DeleteBookService implements Service {
+    async execute(id: string) {
+        try {
+            await prisma.book.delete({
+                where: {
+                    id: id,
+                },
+            });
+            return { message: "Book deleted with success" };
+        } catch (err) {
+            throw new CustomError({ status: 400, message: "Error" });
+        }
+    }
+}
