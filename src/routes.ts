6ed92c750c3ba30a2e8506a46f809be130141c15
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-07 02:30:23 -0300

    Add the user router

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import bookRouter from "./modules/books/book.routes";
+import userRouter from "./modules/users/user.routes";
 
 const routerMapper = Router();
 
 routerMapper.use("/books", bookRouter);
+routerMapper.use("/users", userRouter);
 
 export { routerMapper };
