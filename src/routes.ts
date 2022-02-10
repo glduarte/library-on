c127e8d7626eaa11b7edc7ba74f904811c57277c
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-10 03:34:18 -0300

    Add session router and authorization middlewares to book routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import bookRouter from "./modules/books/book.routes";
+import sessionRouter from "./modules/session/session.routes";
 import userRouter from "./modules/users/user.routes";
+import EnsureAdmin from "./shared/middlewares/ensureAdmin.middleware";
+import EnsureAuthenticated from "./shared/middlewares/ensureAuthenticated.middleware";
+
+const ensureAuthenticated = new EnsureAuthenticated();
+const ensureAdmin = new EnsureAdmin();
 
 const routerMapper = Router();
 
-routerMapper.use("/books", bookRouter);
+routerMapper.use("/books", ensureAuthenticated.handle, ensureAdmin.handle, bookRouter);
 routerMapper.use("/users", userRouter);
+routerMapper.use("/session", sessionRouter);
 
 export { routerMapper };
