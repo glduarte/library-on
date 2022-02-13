87a38dd07106271d8d746db5b14f04ff4027fedf
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-13 02:41:20 -0300

    Add book rent router and delete books middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -2,16 +2,13 @@ import { Router } from "express";
 import bookRouter from "./modules/books/book.routes";
 import sessionRouter from "./modules/session/session.routes";
 import userRouter from "./modules/users/user.routes";
-import EnsureAdmin from "./shared/middlewares/ensureAdmin.middleware";
-import EnsureAuthenticated from "./shared/middlewares/ensureAuthenticated.middleware";
-
-const ensureAuthenticated = new EnsureAuthenticated();
-const ensureAdmin = new EnsureAdmin();
+import bookRentRouter from "./modules/bookRent/bookRent.routes";
 
 const routerMapper = Router();
 
-routerMapper.use("/books", ensureAuthenticated.handle, ensureAdmin.handle, bookRouter);
+routerMapper.use("/books", bookRouter);
 routerMapper.use("/users", userRouter);
 routerMapper.use("/session", sessionRouter);
+routerMapper.use("/book-rent", bookRentRouter);
 
 export { routerMapper };
