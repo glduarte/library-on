f28a426b4823aeebfca9f1d2e46e6567e33fdc14
Guilherme Duarte <gui.lammel.duarte@gmail.com>
2022-02-13 02:42:17 -0300

    Add the middlewares

--- src/modules/books/book.routes.ts
+++ src/modules/books/book.routes.ts
@@ -4,14 +4,22 @@ import DeleteBookController from "./controllers/deleteBook.controller";
 import ShowBooksController from "./controllers/showBooks.controller";
 import UpdateBookController from "./controllers/updateBook.controller";
 
+import EnsureAuthenticated from "../../shared/middlewares/ensureAuthenticated.middleware";
+import EnsureAdmin from "../../shared/middlewares/ensureAdmin.middleware";
+
 const createBookController = new CreateBookController();
 const showBooksController = new ShowBooksController();
 const updateBookController = new UpdateBookController();
 const deleteBookController = new DeleteBookController();
 
+const ensureAuthenticated = new EnsureAuthenticated();
+const ensureAdmin = new EnsureAdmin();
+
 const bookRouter = Router();
 
 bookRouter.get("/", showBooksController.handle);
+
+bookRouter.use(ensureAuthenticated.handle, ensureAdmin.handle);
 
 bookRouter.post("/", createBookController.handle);
 
